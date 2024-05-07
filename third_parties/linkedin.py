3f9eda025e1a1dc1816f2513d1f69fa5a953a1b2
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-05-07 20:54:35 +0200

    added twitter scraping

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,11 +1,9 @@
-import os
-from turtle import screensize
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+def scrape_linkedin_profile(linkedin_profile_url: str):
     """Scrape information from Linkedin profiles
 
     Args:
@@ -13,23 +11,12 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
         mock (bool, optional): _description_. Defaults to False.
     """
 
-    if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
-        response = requests.get(
-            linkedin_profile_url,
-            timeout=10,
-        )
+    linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
+    response = requests.get(
+        linkedin_profile_url,
+        timeout=10,
+    )
     
-    else:
-        api_endpoint = "blank"
-        header_dic = "blank"
-        response = requests.get(
-            api_endpoint,
-            params={"url": linkedin_profile_url},
-            headers=header_dic,
-            timeout=10,
-        )
-
     data = response.json()
 
     data = {
@@ -48,6 +35,5 @@ if __name__ == "__main__":
     print(
         scrape_linkedin_profile(
             linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
-            mock=True,
         )
     )
